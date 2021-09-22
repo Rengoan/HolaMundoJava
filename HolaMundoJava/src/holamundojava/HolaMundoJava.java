fb297a6ae6a611b8bef6ed252742302cd690e09c
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-09-22 09:11:25 +0200

    Update HolaMundoJava.java

--- HolaMundoJava/src/holamundojava/HolaMundoJava.java
+++ HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -17,6 +17,7 @@ public class HolaMundoJava {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo"); //Para visualizar en pantalla
+        System.out.println("Prueba1");
     }
     
 }
